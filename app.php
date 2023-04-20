486b80b2ca44e3fbabc6660bfb5fa52ec1019421
Hein Soe <hi@heinsoe.com>
2023-04-20 14:41:41 +0630

    updated

--- app.php
+++ app.php
@@ -16,7 +16,7 @@
     <!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script> -->
     <!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
 
-
+    <meta name="theme-color" content="#315381" />
     <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <link rel="stylesheet" href="./assets/style.css">
